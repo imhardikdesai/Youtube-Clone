6ba3d688e1e455b41200d83af8660464872a6db1
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-01-31 22:50:35 +0530

    Remove Warnings from Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { BiVideoPlus } from "react-icons/bi";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { BiSearchAlt2 } from "react-icons/bi";
